43fe9e24577b0005b16b6955bfdc49a7c27b7012
NadzGit <nadineseddat@gmail.com>
2025-09-20 15:15:01 +0100

    Co-authored-by: aqsa <axekiller@users.noreply.github.com>

--- studybot.py
+++ studybot.py
@@ -1,9 +1,11 @@
 # bot.py
 import os
+import asyncio
 
 import discord
 from dotenv import load_dotenv
 
+#basic setup of bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD= os.getenv('DISCORD_GUILD')
@@ -28,9 +30,7 @@ async def on_ready():
     )
 
 
-
-
-@client.event
+@client.event #sending dm to a member on join
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f"heyyy {member.name} pooookieeeee :3")
@@ -38,9 +38,34 @@ async def on_member_join(member):
     channel_id=1418226262677393512
     channel = client.get_channel(channel_id)
     if channel:
-        await channel.send(f"HALLOO {member.mention}!")
+        await channel.send(f"HALLOO {member.mention}!") #mentioned in the channel on join
     else:
         print("Something went wrong.")
+
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or isinstance(message.channel, discord.TextChannel): #to prevent loops and to prevent timers being started in the Guild
+        return
+    start_timer = "Your time starts now love! You got this!!" 
+    
+    if "timer" in message.content.lower():
+            await message.channel.send(start_timer)
+    
+    if message.author == client.user:
+        return
+    await asyncio.sleep(6)
+    await message.author.send("Time up queen :3") #works for both dms and in server
+
+    
+
+    
+# @client.command()
+# async def pomodoro_message(ctx):
+#     await asyncio.sleep (5)
+#     await ctx.author.send("Time Up")
 
 
 client.run(TOKEN)

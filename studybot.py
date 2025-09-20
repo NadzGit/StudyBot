3ff5c3e490cb2fec9930fdad322118e4f38bbfdd
NadzGit <nadineseddat@gmail.com>
2025-09-20 14:01:44 +0100

    Initialised bot

--- studybot.py
+++ studybot.py
@@ -8,6 +8,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
+
 client = discord.Client(intents = intents)
 
 @client.event
@@ -15,3 +16,4 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
+print("Staging")
